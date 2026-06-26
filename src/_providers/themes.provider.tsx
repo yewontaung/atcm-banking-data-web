7fe8d90fcb96111ffa9ee83abd72da9ae7b97dc3
coderAung <yewontaung798@gmail.com>
2026-06-27 00:13:52 +0630

    feat: Add themes saved to local storage

--- src/_providers/themes.provider.tsx
+++ src/_providers/themes.provider.tsx
@@ -2,11 +2,20 @@ import { useEffect, useState } from "react";
 import { ThemeContext } from "../_hooks/use-themes";
 
 export function ThemeProvider({children}:{children?:React.ReactNode}) {
-    const [theme, setTheme] = useState<"light" | "dark">("light")
+    const [theme, setTheme] = useState<"light" | "dark">(() => {
+        const savedTheme = localStorage.getItem(`${import.meta.env.VITE_PROJECT_TITLE}_theme`)
+        if(savedTheme === "light" || savedTheme === "dark") {
+            return savedTheme
+        }
+        return "dark"
+    })
     const toggleTheme = () => {setTheme(prev => prev === "light" ? "dark" : "light")}
+
     useEffect(() => {
+        localStorage.setItem(`${import.meta.env.VITE_PROJECT_TITLE}_theme`, theme)
         document.documentElement.setAttribute("data-bs-theme", theme)
     }, [theme])
+
     return (
         <ThemeContext value={{
             theme, toggleTheme
