d7b5d8049d8809d9ed3cfbed47f61189cd9647ab
coderAung <yewontaung798@gmail.com>
2026-06-28 00:13:53 +0630

    feat: Add profile link to main content decorator

--- src/_components/decorators/main-content.tsx
+++ src/_components/decorators/main-content.tsx
@@ -1,15 +1,22 @@
 import type { ReactNode } from "react";
 import ThemeButton from "../theme-button";
+import { Link } from "react-router-dom";
+import { AppProfile } from "../app-profile";
+import { defaultProfile } from "../../_utils/constants";
+import { Container } from "react-bootstrap";
 
 export default function MainContentDecorator({title, children}:{title:string, children:ReactNode}) {
     return (
         <div>
-            <div className="d-flex justify-content-between">
+            <Container className="d-flex justify-content-between">
                 <span className="fw-semibold fs-5">{title}</span>
                 <div className="align-self-start">
                     <ThemeButton />
+                    <Link to={""} className="ms-3">
+                        <AppProfile img={defaultProfile} />
+                    </Link>
                 </div>
-            </div>
+            </Container>
             {children}
         </div>
     )
