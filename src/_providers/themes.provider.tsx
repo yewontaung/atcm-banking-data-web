23bd776798d6c40d5259746ac5e0bbf0aaeda56d
coderAung <yewontaung798@gmail.com>
2026-06-28 00:57:58 +0630

    feat: Update theme switch

--- src/_providers/themes.provider.tsx
+++ src/_providers/themes.provider.tsx
@@ -5,8 +5,10 @@ export function ThemeProvider({children}:{children?:React.ReactNode}) {
     const [theme, setTheme] = useState<"light" | "dark">(() => {
         const savedTheme = localStorage.getItem(`${import.meta.env.VITE_PROJECT_TITLE}_theme`)
         if(savedTheme === "light" || savedTheme === "dark") {
+            document.documentElement.setAttribute("data-bs-theme", savedTheme)
             return savedTheme
         }
+        document.documentElement.setAttribute("data-bs-theme", "dark")
         return "dark"
     })
     const toggleTheme = () => {setTheme(prev => prev === "light" ? "dark" : "light")}
