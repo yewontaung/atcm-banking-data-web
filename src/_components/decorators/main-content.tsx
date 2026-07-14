9e6cea92fabe1f576b40fdaaf8be7f00a8e391a1
coderAung <yewontaung798@gmail.com>
2026-07-15 00:52:25 +0630

    feat: Add dropdown menu on layout

--- src/_components/decorators/main-content.tsx
+++ src/_components/decorators/main-content.tsx
@@ -2,19 +2,32 @@ import type { ReactNode } from "react";
 import ThemeButton from "../theme-button";
 import { Link } from "react-router-dom";
 import { AppProfile } from "../app-profile";
-import { defaultProfile } from "../../_utils/constants";
-import { Container } from "react-bootstrap";
+import { defaultProfile, iconSize } from "../../_utils/constants";
+import { Container, Dropdown } from "react-bootstrap";
+import { LogOutIcon, Trash2Icon, User2Icon } from "lucide-react";
 
 export default function MainContentDecorator({title, children}:{title:string, children:ReactNode}) {
     return (
         <div>
             <Container fluid className="d-flex justify-content-between">
                 <span className="fw-semibold fs-5">{title}</span>
-                <div className="align-self-start">
+                <div className="align-self-start d-flex gap-3">
                     <ThemeButton />
-                    <Link to="/me/profile" className="ms-3">
-                        <AppProfile img={defaultProfile} />
-                    </Link>
+                    <Dropdown>
+                        <Dropdown.Toggle className="bg-transparent border-0">
+                            <AppProfile className="me-2" img={defaultProfile} />
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu className="">
+                            <Dropdown.Item>
+                                <Link to="/me/profile" className="text-decoration-none text-white">
+                                    <User2Icon size={iconSize} className="me-3" /> Profile
+                                </Link>
+                            </Dropdown.Item>
+                            <Dropdown.Item><Trash2Icon size={iconSize} className="me-3" /> Recycle bin</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item  className="text-danger"><LogOutIcon size={iconSize} className="me-3" /> Logout</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
             </Container>
             {children}
