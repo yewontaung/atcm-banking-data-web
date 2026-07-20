69a36319e1f1c3d7c3391f7db5b1b2433e17c7b1
coderAung <yewontaung798@gmail.com>
2026-07-20 23:09:32 +0630

    feat: Prepare for deployment

--- src/_components/decorators/main-content.tsx
+++ src/_components/decorators/main-content.tsx
@@ -1,10 +1,15 @@
-import type { ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
 import { Container } from "react-bootstrap";
 import { getAuthProfile } from "../../_utils/auth.utils";
 import AppNav from "../app-nav";
 
 export default function MainContentDecorator({title, children}:{title:string, children:ReactNode}) {
     const profile = getAuthProfile()
+
+    useEffect(() => {
+        document.title = `${import.meta.env.VITE_PROJECT_TITLE} | ${title}`
+    }, [title])
+
     return (
         <div>
             <Container fluid className="d-flex justify-content-between">
