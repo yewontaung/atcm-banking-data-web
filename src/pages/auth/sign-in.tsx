fee4156b1c90c155832e194fa6148a556b024b63
coderAung <yewontaung798@gmail.com>
2026-07-21 01:24:41 +0630

    feat: Update sign in page for responsive

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button } from "react-bootstrap";
+import { Alert, Button, Container, Row } from "react-bootstrap";
 import { FormsInput } from "../../_components/ui/forms.input";
 import { useForms } from "../../_hooks/use-forms";
 import type { SignInForm } from "../../_models/schemas";
@@ -47,16 +47,20 @@ export default function SignInPage() {
     }
 
     return (
-        <div className="w-50">
-            <h4 className="text-center">Login</h4>
-            <form onSubmit={onSubmit} className="border p-4">
-                {alert && <Alert variant="info">{alert}</Alert>}
-                <FormsInput label="Email" error={errors.accountEmail} name={controls.accountEmail} onChange={onChange} placeholder="Enter Email" className="mb-3"/>
+        <Container fluid>
+            <Row className="justify-content-center">
+                <div className="col-10 col-md-6">
+                    <h4 className="text-center">Login</h4>
+                    <form onSubmit={onSubmit} className="border p-4">
+                        {alert && <Alert variant="info">{alert}</Alert>}
+                        <FormsInput label="Email" error={errors.accountEmail} name={controls.accountEmail} onChange={onChange} placeholder="Enter Email" className="mb-3"/>
 
-                <FormsInput label="Password" error={errors.password} type="password" name={controls.password} onChange={onChange} placeholder="Enter Password" className="mb-3" />
+                        <FormsInput label="Password" error={errors.password} type="password" name={controls.password} onChange={onChange} placeholder="Enter Password" className="mb-3" />
 
-                <Button type="submit" className="w-100" disabled={logginIn}>{logginIn ? "Loggin in..." : "Log In"}</Button>
-            </form>
-        </div>
+                        <Button type="submit" className="w-100" disabled={logginIn}>{logginIn ? "Loggin in..." : "Log In"}</Button>
+                    </form>
+                </div>
+            </Row>
+        </Container>
     )
 }
